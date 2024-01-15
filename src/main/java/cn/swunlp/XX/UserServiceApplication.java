e3e632a7ed0fccbba43a989a79a6e0a7c7dfb85f
唐熙 <tangela1103@foxmail.com>
2024-01-15 11:06:40 +0800

    init:初始化模板工程

---
+++ src/main/java/cn/swunlp/XX/UserServiceApplication.java
@@ -0,0 +1,16 @@
+package cn.swunlp.XX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author TangXi
+ */
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
---
+++ src/main/java/cn/swunlp/XX/config/WebConfig.java
@@ -0,0 +1,10 @@
+package cn.swunlp.XX.config;
+
+/**
+ * @author TangXi
+ * @since 2023/12/26
+ */
+
+public class WebConfig {
+    // Your class implementation goes here
+}
